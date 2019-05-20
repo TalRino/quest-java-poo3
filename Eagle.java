e00da0c316ae778136a38fb1a04725593f3f14f3
Loup BINA <binaloup@gmail.com>
2019-05-20 17:37:39 +0200

    Add file for the quest 8 to Java from WCS.

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -17,8 +17,54 @@ public class Eagle extends Bird {
         return flying;
     }
 
+    public void setAltitude(int altitude) {
+        this.altitude = altitude;
+    }
+
+    public void setFlying(boolean flying) {
+        this.flying = flying;
+    }
+
+
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff() {
+        if (!isFlying()) {
+            this.setFlying(true);
+            System.out.println(this.getName() + " takes off in the sky.");
+        } else {
+            System.out.println(this.getName() + " is already off in th sky!");
+        }
+    }
+
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude + meters, 0);
+            System.out.printf("%s flies upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.printf("%s flies downward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void land() {
+        if (this.flying && this.altitude <= 1) {
+            System.out.println(this.getName() + " lands on the ground");
+        } else {
+            System.out.println(this.getName() + " is too high, it can't lands.");
+        }
     }
 }
